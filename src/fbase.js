042c9e6daa3e53747aae8a55753d5bb5d9c701b1
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-01-05 20:32:56 +0900

    nweet form & Database Setup

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import  { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
+import "firebase/database";
 
 
 const firebaseConfig = {
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 
 
-const Home = ()=><span>Home</span>
+const Home = ()=>{
+    const [nweet, setNweet] = useState("");
+    const onSubmit =(ev)=>{
+        ev.preventDefault();
+    };
+    const onChange = (ev)=>{
+        const{target:{value}} = ev;
+        setNweet(value);
+
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120}/>
+                <input type="submit" value="Nweet"/>
+            </form>
+        </div>
+
+    );
+    };
 export default Home;
