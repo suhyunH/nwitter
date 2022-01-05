ca6365a7c2de075aa811dc65caeddb219802ad7b
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-01-05 21:31:45 +0900

    send a data to fireStore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,32 @@
 import React,{useState} from "react";
+import { dbService } from "../fbase";
+import { addDoc, collection,serverTimestamp  } from "firebase/firestore";
 
 
 const Home = ()=>{
     const [nweet, setNweet] = useState("");
-    const onSubmit =(ev)=>{
-        ev.preventDefault();
-    };
-    const onChange = (ev)=>{
-        const{target:{value}} = ev;
-        setNweet(value);
 
-    }
+    const onSubmit =async (e) => {
+        e.preventDefault();
+        console.log(`서브밋 하는 느윗:${nweet}`);
+        await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: serverTimestamp(),
+        });
+        setNweet("");
+        };
+
+
+    const onChange = (ev)=>{
+        const{
+            target:{value},
+        } = ev;
+        setNweet(value);
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120}/>
+                <input value={nweet} type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120}/>
                 <input type="submit" value="Nweet"/>
             </form>
         </div>
